112eceac3a2041d416f0b8691db03bdd590f9e33
IMarelja <imarelja@algebra.hr>
2025-06-10 22:55:04 +0200

    Removing Procedures

--- SQLScript/SQLScript.sql
+++ SQLScript/SQLScript.sql
@@ -9,27 +9,3 @@ CREATE TABLE [User] (
     Username NVARCHAR(100) UNIQUE NOT NULL,
     Password NVARCHAR(255) NOT NULL
 );
-
--- Get User
-CREATE PROCEDURE GetUserByUsername
-    @Username NVARCHAR(50)
-AS
-BEGIN
-    SELECT Usery.ID, Usery.Username, Usery.Password, Roles.RoleName
-    FROM [User] Usery
-	JOIN Roles ON Usery.RoleID = Roles.ID
-    WHERE Usery.Username = @Username
-END
-GO
-
--- Create User
-CREATE PROCEDURE CreateUser
-    @Username NVARCHAR(100),
-    @Password NVARCHAR(255)
-    @ID INT OUTPUT
-AS
-BEGIN
-    VALUES (@Username, @Password);
-	SET @ID = SCOPE_IDENTITY();
-END
-GO
