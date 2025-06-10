ea50e913b259dd8d1ee8e9ca8840334efa258f75
IMarelja <imarelja@algebra.hr>
2025-06-10 22:26:01 +0200

    Middle ware database commit

---
+++ Project/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MiddleWareSimulation.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
