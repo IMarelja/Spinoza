4a34272731fba66de200077024ff41f8ddf62b97
IMarelja <imarelja@algebra.hr>
2025-06-11 19:59:22 +0200

    (Only a template) Authentication file for auto login

---
+++ Project/Display/Utilities/Utility.cs
@@ -0,0 +1,21 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display.Utilities
+{
+    public class Utility
+    {
+        //
+        // Saving logged in user on a file and he will automatically log in after starting up the app
+        //
+        public static void SaveLoginToFile(AuthLogin loginData)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
