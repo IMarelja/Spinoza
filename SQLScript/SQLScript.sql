7eb7c8ee8d266bfcee4ea072ff95ce279ebd5d92
IMarelja <imarelja@algebra.hr>
2025-06-11 17:32:40 +0200

    Adding back procedures

--- SQLScript/SQLScript.sql
+++ SQLScript/SQLScript.sql
@@ -9,3 +9,27 @@ CREATE TABLE [User] (
     Username NVARCHAR(100) UNIQUE NOT NULL,
     Password NVARCHAR(255) NOT NULL
 );
+
+CREATE PROCEDURE CreateUser
+    @Username NVARCHAR(100),
+    @Password NVARCHAR(255),
+    @ID INT OUTPUT
+AS
+BEGIN
+    INSERT INTO [User] (Username, Password)
+    VALUES (@Username, @Password);
+
+    SET @ID = SCOPE_IDENTITY();
+END
+GO
+
+-- Get User
+CREATE PROCEDURE GetUserByUsername
+    @Username NVARCHAR(50)
+AS
+BEGIN
+    SELECT Usery.ID, Usery.Username, Usery.Password
+    FROM [User] Usery
+    WHERE Usery.Username = @Username
+END
+GO
