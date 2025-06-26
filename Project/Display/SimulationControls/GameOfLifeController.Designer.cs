03b0181c0d3621147008b3132a14b72e162e9ad0
gizemozdogn <gozdogan@algebra.hr>
2025-06-26 21:19:25 +0200

    User Control Update

---
+++ Project/Display/SimulationControls/GameOfLifeController.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Display
+{
+    partial class GameOfLifeController
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameOfLifeController));
+            nudRowsTable = new NumericUpDown();
+            nudColumnsTable = new NumericUpDown();
+            nudSeedCells = new NumericUpDown();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            lblRows = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            btnStart = new Button();
+            btnPause = new Button();
+            btn = new Button();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)nudRowsTable).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudColumnsTable).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudSeedCells).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            SuspendLayout();
+            // 
+            // nudRowsTable
+            // 
+            nudRowsTable.Location = new Point(99, 55);
+            nudRowsTable.Name = "nudRowsTable";
+            nudRowsTable.Size = new Size(132, 27);
+            nudRowsTable.TabIndex = 0;
+            // 
+            // nudColumnsTable
+            // 
+            nudColumnsTable.Location = new Point(99, 88);
+            nudColumnsTable.Name = "nudColumnsTable";
+            nudColumnsTable.Size = new Size(132, 27);
+            nudColumnsTable.TabIndex = 1;
+            // 
+            // nudSeedCells
+            // 
+            nudSeedCells.Location = new Point(99, 121);
+            nudSeedCells.Name = "nudSeedCells";
+            nudSeedCells.Size = new Size(132, 27);
+            nudSeedCells.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(240, 55);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(25, 27);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.notify;
+            pictureBox2.Location = new Point(240, 88);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(25, 27);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.notify;
+            pictureBox3.Location = new Point(240, 121);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(25, 27);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            // 
+            // lblRows
+            // 
+            lblRows.AutoSize = true;
+            lblRows.Location = new Point(15, 55);
+            lblRows.Name = "lblRows";
+            lblRows.Size = new Size(44, 20);
+            lblRows.TabIndex = 6;
+            lblRows.Text = "Rows";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Columns";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(16, 122);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Seed";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(17, 445);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 9;
+            button1.Text = "Initialize";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(17, 480);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 10;
+            button2.Text = "Randomize";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(17, 515);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 11;
+            button3.Text = "Import";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 20);
+            label1.TabIndex = 12;
+            label1.Text = "Game Of Life";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.notify;
+            pictureBox4.Location = new Point(117, 447);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(25, 27);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 13;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.notify;
+            pictureBox5.Location = new Point(117, 480);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(25, 27);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 14;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.notify;
+            pictureBox6.Location = new Point(117, 517);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(25, 27);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 15;
+            pictureBox6.TabStop = false;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(52, 190);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(51, 28);
+            btnStart.TabIndex = 16;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            btnPause.Location = new Point(108, 190);
+            btnPause.Name = "btnPause";
+            btnPause.Size = new Size(56, 28);
+            btnPause.TabIndex = 17;
+            btnPause.Text = "Pause";
+            btnPause.UseVisualStyleBackColor = true;
+            // 
+            // btn
+            // 
+            btn.Location = new Point(170, 190);
+            btn.Name = "btn";
+            btn.Size = new Size(56, 28);
+            btn.TabIndex = 18;
+            btn.Text = "Step";
+            btn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(67, 224);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(25, 27);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 19;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(127, 224);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(25, 27);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 20;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(188, 224);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(25, 27);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 21;
+            pictureBox9.TabStop = false;
+            // 
+            // GameOfLifeController
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(btn);
+            Controls.Add(btnPause);
+            Controls.Add(btnStart);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(lblRows);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(nudSeedCells);
+            Controls.Add(nudColumnsTable);
+            Controls.Add(nudRowsTable);
+            Name = "GameOfLifeController";
+            Size = new Size(276, 569);
+            ((System.ComponentModel.ISupportInitialize)nudRowsTable).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudColumnsTable).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudSeedCells).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private NumericUpDown nudRowsTable;
+        private NumericUpDown nudColumnsTable;
+        private NumericUpDown nudSeedCells;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label lblRows;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private Button btnStart;
+        private Button btnPause;
+        private Button btn;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+    }
+}
---
+++ Project/Display/SimulationControls/GameOfLifeController.cs
@@ -0,0 +1,119 @@
+﻿using Display.Utilities;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.Marshalling.IIUnknownCacheStrategy;
+
+namespace Display
+{
+    public partial class GameOfLifeController : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private Life _simulation;
+        private System.Windows.Forms.Timer _timer;
+        private int _cellSize = 15;
+
+        public Life Simulation
+        {
+            get => _simulation;
+            set
+            {
+                _simulation = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Simulation)));
+                Invalidate();
+            }
+        }
+
+        public GameOfLifeController()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Interval = 250;
+            _timer.Tick += (s, e) => Advance();
+        }
+
+        public void InitializeGrid(int cols, int rows, int seedCells = 100)
+        {
+            Simulation = new Life(cols, rows, seedCells);
+            Width = cols * _cellSize + 1;
+            Height = rows * _cellSize + 1;
+        }
+
+        public void Advance()
+        {
+            if (Simulation == null) return;
+            Simulation.NextStep();
+            Invalidate();
+        }
+
+        public void Back()
+        {
+            if (Simulation == null) return;
+            Simulation.BackStep();
+            Invalidate();
+        }
+
+        public void Start() => _timer.Start();
+        public void Pause() => _timer.Stop();
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (Simulation == null) return;
+
+            var g = e.Graphics;
+            var state = Simulation.CurrentState();
+
+            for (int y = 0; y < state.GetLength(1); y++)
+            {
+                for (int x = 0; x < state.GetLength(0); x++)
+                {
+                    var rect = new Rectangle(x * _cellSize, y * _cellSize, _cellSize, _cellSize);
+                    g.FillRectangle(state[x, y] == 1 ? Brushes.Black : Brushes.White, rect);
+                    g.DrawRectangle(Pens.LightGray, rect);
+                }
+            }
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+
+            if (Simulation == null) return;
+
+            int x = e.X / _cellSize;
+            int y = e.Y / _cellSize;
+
+            var current = Simulation.CurrentState();
+
+            if (x < current.GetLength(0) && y < current.GetLength(1))
+            {
+                var newModel = new Life(current.GetLength(0), current.GetLength(1), 0);
+                var newGrid = Simulation.CurrentState();
+
+                for (int j = 0; j < newGrid.GetLength(1); j++)
+                    for (int i = 0; i < newGrid.GetLength(0); i++)
+                        if (i != x || j != y)
+                            newModel.NextStep(); // or assign manually if needed
+
+                newModel = new Life(current.GetLength(0), current.GetLength(1), 0);
+                for (int i = 0; i < current.GetLength(0); i++)
+                    for (int j = 0; j < current.GetLength(1); j++)
+                        newModel.NextStep();
+
+                Invalidate();
+            }
+        }
+    }
+}
