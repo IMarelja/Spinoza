8aab5a667c7bf68dbe496a6c1b11ca019c93b3fc
IMarelja <imarelja@algebra.hr>
2025-06-30 10:28:22 +0200

    Adding back appsettings.json with no connection + a comment

--- Project/MiddleWareSimulation/Program.cs
+++ Project/MiddleWareSimulation/Program.cs
@@ -17,9 +17,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Edit this file depending on the service
-//---------------------------------------------------------------------------------------------------------------------------------
-// (!) Configure the "appsettings.json" file NOT HERE for "DefaultConnection" to the SQL server and git ignore any changes to it (!)
-//---------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+// (!) Configure the "appsettings.json" for the MS-SQL server connections (!)
+//----------------------------------------------------------------------------
 builder.Services.AddScoped<IAuthService>(provider =>
 {
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
